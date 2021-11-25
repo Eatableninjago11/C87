43dd3b0963f73a59526d4fa88d1456bd921631d8
Eatableninjago11 <83182611+Eatableninjago11@users.noreply.github.com>
2021-11-25 11:38:22 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkwIPMKlPEhPivWUaKRfsNgU9xkkLHor0",
+  authDomain: "storytelling-app-75f25.firebaseapp.com",
+  databaseURL: "https://storytelling-app-75f25-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-75f25",
+  storageBucket: "storytelling-app-75f25.appspot.com",
+  messagingSenderId: "22965238701",
+  appId: "1:22965238701:web:5f62831c09458fcc47ff24"
+};
